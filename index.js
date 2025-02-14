cbe669b3e3362d30e9cd817cf67c0e964d7a1583
tanjro77 <mohamedprograming99@gmail.com>
2025-02-14 21:44:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// Simple JavaScript functionality for future enhancements
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Welcome to Adam Mohamed GD's website!");
+});
